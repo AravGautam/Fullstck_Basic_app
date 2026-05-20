e2a23071b01fb97a8f816cbab64c34c0955f6fbc
Arav Gautam <aravg@dream-filler.com>
2026-05-20 17:18:23 +0530

    Added rate limitiing and a docker file - it's useless

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,6 +5,39 @@ const postRoutes = require("./routes/post.routes")
 const app = express();  
 app.use(cors())
 app.use(express.json())
-app.use("/api/", postRoutes)
+
+
+const requestStore = {};
+
+function rateLimiter(req, res, next) {
+    const ip = req.ip;
+    const now = Date.now();
+    const windowTime = 30 * 1000; // 1 minute
+    console.log(req.method, req.url, req.path);
+    console.log("Rate limiter called for IP:", ip);
+    console.log("Current request timestamps for IP:", requestStore[ip], requestStore);
+
+    if (!requestStore[ip]) {
+        requestStore[ip] = [];
+    }
+
+    requestStore[ip] = requestStore[ip].filter(
+        timestamp => now - timestamp < windowTime
+    );
+
+    requestStore[ip].push(now);
+
+    console.log(requestStore[ip]);
+
+    if (requestStore[ip].length > 10) {
+        return res.status(429).json({
+            error: "Too many requests"
+        });
+    }
+
+    next();
+}
+
+app.use("/api/", rateLimiter, postRoutes)
 
 module.exports = app
