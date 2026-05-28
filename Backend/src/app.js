0f4ff2a92b8582e52ae6508693a777b2140f34e5
Arav Gautam <aravg@dream-filler.com>
2026-05-28 17:15:35 +0530

    Throttling added

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const cors = require("cors");
-const postRoutes = require("./routes/post.routes")
+const postRoutes = require("./routes/post.routes");
 
-const app = express();  
+const app = express();
 app.use(cors())
 app.use(express.json())
 
+// ---------------- RATE LIMITER ----------------
 
 const requestStore = {};
 
 function rateLimiter(req, res, next) {
     const ip = req.ip;
     const now = Date.now();
-    const windowTime = 30 * 1000; // 1 minute
-    console.log(req.method, req.url, req.path);
-    console.log("Rate limiter called for IP:", ip);
-    console.log("Current request timestamps for IP:", requestStore[ip], requestStore);
+    const windowTime = 60 * 1000; // 1 minute
+    // console.log(req.method, req.url, req.path);
+    // console.log("Rate limiter called for IP:", ip);
+    // console.log("Current request timestamps for IP:", requestStore[ip], requestStore);
 
     if (!requestStore[ip]) {
         requestStore[ip] = [];
@@ -26,10 +27,10 @@ function rateLimiter(req, res, next) {
     );
 
     requestStore[ip].push(now);
+    console.log('Rate limmmtiing rn bitch stwwaapp')
+    console.log("Requests:", requestStore[ip]);
 
-    console.log(requestStore[ip]);
-
-    if (requestStore[ip].length > 10) {
+    if (requestStore[ip].length > 5) {
         return res.status(429).json({
             error: "Too many requests"
         });
@@ -38,6 +39,43 @@ function rateLimiter(req, res, next) {
     next();
 }
 
-app.use("/api/", rateLimiter, postRoutes)
 
-module.exports = app
+// ---------------- THROTTLE ----------------
+
+const throttleStore = {};
+
+function throttleMiddleware(req, res, next) {
+    const ip = req.ip;
+    const now = Date.now();
+    const delay = 5 * 1000; // 5 sec
+
+    // first request
+    if (!throttleStore[ip]) {
+        throttleStore[ip] = now;
+        return next();
+    }
+    
+    console.log("Throttle middleware called for:", ip);
+    console.log(throttleStore)
+
+    const timePassed = now - throttleStore[ip];
+    console.log(timePassed)
+    // block if request too fast
+    if (timePassed < delay) {
+        console.log("Wait nigga..!!")
+        return res.status(429).json({
+            error: `Please wait ${Math.ceil((delay - timePassed) / 1000)} seconds`
+        });
+    }
+
+    // update latest request time
+    throttleStore[ip] = now;
+    next();
+}
+
+
+// ---------------- ROUTES ----------------
+
+app.use("/api/", throttleMiddleware, rateLimiter, postRoutes);
+
+module.exports = app;
--- Frontend/src/pages/Feed.jsx
+++ Frontend/src/pages/Feed.jsx
@@ -5,7 +5,7 @@ const Feed = () => {
   const [posts, setPosts] = useState([{
     _id: "1",
     image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-    caption: "hello",
+    caption: "OI mate, jut wait while we load the data there, right! thank uhh.. ",
   }]);
 
   useEffect(() => {
